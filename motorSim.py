6c34b4f339725e4230e34f494405210323d96c48
mahoep <mahoep@users.noreply.github.com>
2018-07-02 12:30:08 -0700

    gas dynamics

--- motorSim.py
+++ motorSim.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from math import pi
+from scipy.optimize import fsolve
+
+
+
 
 
 class SolidMotor:
@@ -8,6 +12,16 @@ class SolidMotor:
         self.grains = grains
         self.propellant = propellant
         self.nozzle = nozzle
+
+    def Astar(self, M):
+        gamma = self.propellant.gamma
+        return 1 / M * \
+    (((gamma - 1) / 2 * M ** 2) / ((gamma) / 2)) ** \
+    ((gamma + 1) / (2 * (gamma - 1))) - self.nozzle.exit_diameter**2 / self.nozzle.throat_diameter**2
+
+    def Tstar(self, M):
+        gamma = self.propellant.gamma
+        return 1/(1 + ((gamma -1)/2)*M**2)
 
     def simulate(self):
         pressure = 14.7
@@ -19,11 +33,14 @@ class SolidMotor:
         time = 0
         pressure_data = []
         time_data = []
+
         while 1:
             burnArea = (pi/4 * self.grains.diameter**2 - pi/4 * self.grains.core_diameter**2) * 2 * self.grains.num + \
                        self.grains.core_diameter * pi * self.grains.length * self.grains.num
 
             amount_burned = self.propellant.a * pressure ** self.propellant.n * dt
+
+            mdot = burnArea * self.propellant.a * pressure**self.propellant.n * self.propellant.density * dt
 
             pressure = (burnArea / throatArea * self.propellant.density * self.propellant.cstar * self.propellant.a*12/386.06) ** \
                        (1 / (1 - self.propellant.n))
@@ -32,7 +49,12 @@ class SolidMotor:
             self.grains.length -= amount_burned*2
 
 
-            time += dt
+            exitMach = fsolve(self.Astar, 2)
+            exitT = 1/(1 + ((self.propellant.gamma -1)/2)*exitMach**2) * self.propellant.T
+            soundSpeed = (self.propellant.gamma * exitT * self.propellant.R)
+            exitVel = soundSpeed*exitMach
+            exitPress = pressure* (1/(1 + ((self.propellant.gamma-1)/2)*exitMach[0]**2)) ** (self.propellant.gamma/(self.propellant.gamma-1))
+            thrust = mdot*exitVel - (self.nozzle.exit_diameter**2 * pi/4)*(exitPress-14.7)
             pressure_data.append(pressure)
             time_data.append(time)
             if self.grains.core_diameter >= self.grains.diameter:
@@ -56,6 +78,8 @@ class Propellant:
         self.gamma = gamma
         self.cstar = cstar
         self.mol_mass = mol_mass
+        self.T = 4500 #Rankine
+        self.R = 1545.3489/mol_mass
 
 
 class Nozzle:
